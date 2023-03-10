6df64405c5017cd1e8d4f06b85835c43d319ca09
Yuriy Koziy <yukoziy@gmail.com>
2023-03-09 19:12:59 -0500

    clean up unused imports

--- src/main/java/com/yuriykoziy/issueTracker/repositories/CommentRepository.java
+++ src/main/java/com/yuriykoziy/issueTracker/repositories/CommentRepository.java
@@ -4,7 +4,6 @@ import com.yuriykoziy.issueTracker.models.Comment;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
--- src/main/java/com/yuriykoziy/issueTracker/repositories/IssueRepository.java
+++ src/main/java/com/yuriykoziy/issueTracker/repositories/IssueRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface IssueRepository extends PagingAndSortingRepository<Issue, Long> {
