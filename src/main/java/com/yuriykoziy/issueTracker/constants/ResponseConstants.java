c1bdd588cdedca18b49911bf7af520347e0d8e0b
Yuriy Koziy <yukoziy@gmail.com>
2023-03-09 19:46:44 -0500

    Added response constants

---
+++ src/main/java/com/yuriykoziy/issueTracker/constants/ResponseConstants.java
@@ -0,0 +1,14 @@
+package com.yuriykoziy.issueTracker.constants;
+
+public class ResponseConstants {
+  private ResponseConstants() {
+  }
+
+  public static final String ISSUES = "issues";
+  public static final String USERS = "users";
+  public static final String COMMENTS = "comments";
+  public static final String NUMBER = "number";
+  public static final String TOTAL_ELEMENTS = "totalElements";
+  public static final String TOTAL_PAGES = "totalPages";
+  public static final String SIZE = "size";
+}
